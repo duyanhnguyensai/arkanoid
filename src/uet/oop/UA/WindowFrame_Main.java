0f7c3b8358454ed644bc5dc789a373e7a3230c0f
duckhiem5a3-hue <duckhiem5a3@gmail.com>
2025-10-17 11:34:09 +0700

    feat: create a working panel on the frame

--- src/uet/oop/UA/WindowFrame_Main.java
+++ src/uet/oop/UA/WindowFrame_Main.java
@@ -1,9 +1,37 @@
 package uet.oop.UA;
 
-import javax.swing.*;
+import uet.oop.UA.entites.Ball;
+import uet.oop.UA.entites.GameObject;
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+class Main_Panel extends JPanel {
+    private List<GameObject> objectList;
 
+    public Main_Panel  (List<GameObject> objects){
+        this.objectList = objects;
+        this.setLayout(new BorderLayout());
+        this.setBackground(Color.black);
+        this.setBounds(0, 0, WindowFrame_Main.WIN_WIDTH, WindowFrame_Main.WIN_HEIGHT);
+    }
+    public void addGameObject(GameObject gameObject){
+        this.objectList.add(gameObject);
+    }
+    public void removeGameObject(GameObject gameObject){
+        this.objectList.remove(gameObject);
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (GameObject obj : objectList) {
+            if (obj.getImage() != null) {
+                g.drawImage(obj.getImage(), obj.getX(), obj.getY(), this);
+            }
+        }
+    }
+}
 
 
 public class WindowFrame_Main extends JFrame
@@ -12,8 +40,9 @@ public class WindowFrame_Main extends JFrame
     public static final int WIN_HEIGHT = 800;
     /*
      * để sử dụng các class khác trong project,
-     * cần tạo đối tượng của class đó trong class này
+     * cần tạo đối tượng của các class đó trong class này
      * và add nó vào frame
+     *
      * để class này có thể sử dụng các object tham chiếu đó
      * cần phải đặt các class của chúng là fields của class này
      * và khởi tạo chúng trong constructor
@@ -21,13 +50,24 @@ public class WindowFrame_Main extends JFrame
     //khởi tạo các fields ở đây
     //ví dụ:
     //private Class_panel panel_ref;
+    private Main_Panel mainPanel;
 
     //constructor
     public WindowFrame_Main()
     {
-        //khởi tạo các object tham chiếu ở đây
-        //ví dụ:
-        //panel_ref = new Class_panel();
+        //tạo list chứa game object
+        //tạo object panel chứa list game object
+        List<GameObject> Objects = new ArrayList<>();
+        Main_Panel mainPanel = new Main_Panel(Objects);
+        //tạo và add các game object vào game object list ở đây
+        //GameObject obj1 = ...
+        //main_Panel.addGameObject( obj1 );
+        //main_Panel.removeGameObject ( obj1 );
+
+
+        this.add(mainPanel);
+        this.pack();
+
         this.setTitle("arkanoid");
         this.setSize(WIN_WIDTH, WIN_HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,9 +76,7 @@ public class WindowFrame_Main extends JFrame
         this.setVisible(true);
         this.setLayout(new BorderLayout());
         this.setBackground(Color.BLACK);
-        //add các object tham chiếu vào frame ở đây
-        //ví dụ:
-        //this.add(panel_ref);
+
     }
     //main method
 
@@ -46,6 +84,7 @@ public class WindowFrame_Main extends JFrame
 
     public static void main(String[] args)
     {
+        //SwingUtilities.invokeLater(() -> new WindowFrame_Main());
         new WindowFrame_Main();
     }
 }
