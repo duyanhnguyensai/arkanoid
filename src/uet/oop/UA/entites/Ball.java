76722bc84439aa354d241ca0b5c5530f75e6372b
duyanhnguyensai <duyanhnguyensai@gmail.com>
2025-10-18 23:38:02 +0700

    feat: adding field motionAngle, cosAngle, sinAngle for class Ball

--- src/uet/oop/UA/entites/Ball.java
+++ src/uet/oop/UA/entites/Ball.java
@@ -3,4 +3,7 @@ package uet.oop.UA.entites;
 public class Ball extends MovableObject {
     double directionX;
     double directionY;
+    double motionAngle;
+    double cosaAngle;
+    double sinAngle;
 }
