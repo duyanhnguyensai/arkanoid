e949413692d41decb4b724690766616cffa82b0e
duckhiem5a3-hue <duckhiem5a3@gmail.com>
2025-10-18 23:01:56 +0700

    fix: merge Khiem' gameObject drawing into V.Anh panel (part1)

--- src/uet/oop/UA/GamePanel.java
+++ src/uet/oop/UA/GamePanel.java
@@ -1,11 +1,14 @@
 package uet.oop.UA;
 
+import uet.oop.UA.entites.GameObject;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon; // load ảnh
 import java.io.File; // tạo file
+import java.util.List;
 
 
 /**
@@ -14,129 +17,32 @@ import java.io.File; // tạo file
  * - thiết kế màn chơi (hiển thị brick, paddle, score, lives, level, game over)
  */
 class GamePanel extends JPanel implements KeyListener {
-    // Thông số cơ bản
-    private final int GAME_WIDTH = 800;
-    private final int GAME_HEIGHT = 600;
-    private final int PADDLE_WIDTH = 150;
-    private final int PADDLE_HEIGHT = 40;
-    private final int BRICK_WIDTH = 90;
-    private final int BRICK_HEIGHT = 30;
+    //Danh sách các vật thể trong một panel
+    private List<GameObject> objectList;
 
-    // vị trí paddle
-    private int paddleX = GAME_WIDTH / 2 - PADDLE_WIDTH / 2;
-
-    // lưu ảnh paddle
-    private Image paddleImage;
-
-
-    // Game state
-    private int score = 0;
-    private int lives = 3;
-    private int level = 1;
-    private boolean gameRunning = true;
-
-    // Khởi tạo và vẽ Bricks (hàng x cột)
-    private boolean[][] bricks;
-    private final int BRICK_ROWS = 4;
-    private final int BRICK_COLS = 8;
-
-    //Khởi tạo Game
-    public GamePanel() {
-        setBackground(Color.BLACK); //màu nền
-        initializeBricks(); //vẽ Bricks
-        loadPaddleImage(); //load ảnh paddle
-        addKeyListener(this); //nhận phím
+    //thêm/bỏ vật thể ra khỏi danh sách
+    public void addGameObject(GameObject gameObject){
+        this.objectList.add(gameObject);
     }
+    public void removeGameObject(GameObject gameObject){
+        this.objectList.remove(gameObject);
 
-    //Khởi tạo Bricks
-    private void initializeBricks() {
-        bricks = new boolean[BRICK_ROWS][BRICK_COLS];
-        for (int i = 0; i < BRICK_ROWS; i++) {
-            for (int j = 0; j < BRICK_COLS; j++) {
-                bricks[i][j] = true; // true = gạch còn, false = gạch đã phá
-            }
-        }
-    }
-
-    //Load ảnh paddle từ resources
-    private void loadPaddleImage() {
-        java.net.URL resource = getClass().getClassLoader().getResource("paddleImage/paddle.png");
-        if (resource != null) {
-            paddleImage = new ImageIcon(resource).getImage();
-            System.out.println("Đã load ảnh paddle thành công");
-            return;
-        }
-
-        File imageFile = new File("res/paddleImage/paddle.png");
-        if (imageFile.exists()) {
-            paddleImage = new ImageIcon(imageFile.getPath()).getImage();
-            System.out.println("Đã load ảnh paddle thành công");
-            return;
-        }
-
-        System.out.println("Không tìm thấy ảnh paddle");
-    }
-
-    //Vẽ Game
+    //hàm vẽ
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (gameRunning) {
-            drawGame(g);
-        } else {
-            drawGameOver(g);
-        }
-    }
-
-    private void drawGame(Graphics g) {
-        // Vẽ paddle
-        drawPaddle(g);
-
-        // Vẽ Bricks
-        drawBricks(g);
-
-        // Vẽ thông tin game (score, lives, level)
-        drawGameInfo(g);
-    }
-
-    //Vẽ Paddle từ ảnh
-    private void drawPaddle(Graphics g) {
-        int paddleY = GAME_HEIGHT - 60;
-
-        if (paddleImage != null) {
-            // Vẽ ảnh paddle
-            g.drawImage(paddleImage, paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT, null);
-        } else {
-            // Vẽ paddle đơn giản nếu không có ảnh
-            g.setColor(Color.WHITE);
-            g.fillRect(paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
-        }
-    }
-
-    //Vẽ Bricks
-    private void drawBricks(Graphics g) {
-        for (int i = 0; i < BRICK_ROWS; i++) {
-            for (int j = 0; j < BRICK_COLS; j++) {
-                if (bricks[i][j]) {
-                    int x = j * BRICK_WIDTH + 20;
-                    int y = i * BRICK_HEIGHT + 50;
-
-                    //Màu Bricks
-                    switch (i) {
-                        case 0 -> g.setColor(Color.RED);
-                        case 1 -> g.setColor(Color.YELLOW);
-                        case 2 -> g.setColor(Color.BLUE);
-                        case 3 -> g.setColor(Color.GREEN);
-                    }
-                    g.fillRect(x, y, BRICK_WIDTH - 5, BRICK_HEIGHT - 2);
-                    g.setColor(Color.WHITE); //màu viền Bricks
-                    g.drawRect(x, y, BRICK_WIDTH - 5, BRICK_HEIGHT - 2);
+        if(gameRunning) {
+            for (GameObject obj : objectList) {  //vẽ gameObject trong list
+                if (obj.getImage() != null) {
+                    g.drawImage(obj.getImage(), obj.getX(), obj.getY(), this);
                 }
             }
+            drawGameInfo(g);  // vẽ chữ
+        }
+        else {
+            drawGameOver(g); //vẽ màn gameover
         }
     }
-
-    //Vẽ thông tin game (score, lives, level)
     private void drawGameInfo(Graphics g) {
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 16));
@@ -155,7 +61,39 @@ class GamePanel extends JPanel implements KeyListener {
         g.drawString("GAME OVER", GAME_WIDTH / 2 - 150, GAME_HEIGHT / 2 - 50); //text Game Over
 
     }
-    
+
+
+    //drawgameInfo và drawgameOver được tôi tích hợp vào paintComponent mới của tôi
+    //bên trên là phần tôi bổ sung . Bn dưới là phần cũ của ông, tôi đã xóa 1 phần ve obj
+    private final int GAME_WIDTH = 800;
+    private final int GAME_HEIGHT = 600;
+    private final int PADDLE_WIDTH = 150;
+    private final int PADDLE_HEIGHT = 40;
+    private final int BRICK_WIDTH = 90;
+    private final int BRICK_HEIGHT = 30;
+
+    // Game state
+    private int score = 0;
+    private int lives = 3;
+    private int level = 1;
+    private boolean gameRunning = true;
+    /*
+    // Khởi tạo và vẽ Bricks (hàng x cột)
+    //private boolean[][] bricks;
+    private final int BRICK_ROWS = 4;
+    private final int BRICK_COLS = 8;
+    */
+    //Khởi tạo Game
+    public GamePanel(List<GameObject> objects) {
+        this.objectList = objects;
+        setBackground(Color.BLACK); //màu nền
+        this.setLayout(new BorderLayout());
+        //initializeBricks(); //vẽ Bricks
+        //loadPaddleImage(); //load ảnh paddle
+        addKeyListener(this); //nhận phím
+    }
+
+    //Vẽ thông tin game (score, lives, level)
     //Nhận phím
     @Override
     public void keyPressed(KeyEvent e) {
@@ -192,7 +130,6 @@ class GamePanel extends JPanel implements KeyListener {
         lives = 3;
         level = 1;
         gameRunning = true;
-        initializeBricks();
         repaint();
     }
 }
