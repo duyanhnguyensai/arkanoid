2d19ae2ba15d7acaf026308945c5528b9b8f5d11
duyanhnguyensai <duyanhnguyensai@gmail.com>
2025-10-19 00:00:23 +0700

    feat: adding method move(double angle) for class Ball

--- src/uet/oop/UA/entites/Ball.java
+++ src/uet/oop/UA/entites/Ball.java
@@ -1,9 +1,26 @@
 package uet.oop.UA.entites;
 
+import static java.lang.Math.*;
+
+
 public class Ball extends MovableObject {
     double directionX;
     double directionY;
     double motionAngle;
     double cosaAngle;
     double sinAngle;
+
+    public void move(double angle) {
+        this.motionAngle = angle;
+        this.cosaAngle = cos(this.motionAngle);
+        this.sinAngle = sin(this.motionAngle);
+        this.directionX = this.speed
+                * (this.cosaAngle / sqrt(this.cosaAngle * this.cosaAngle
+                + this.sinAngle * this.sinAngle));
+        this.directionY = this.speed
+                * (this.sinAngle / sqrt(this.sinAngle * this.sinAngle
+                + this.cosaAngle * this.cosaAngle));
+        this.setX(this.getX()+ (int) this.directionX);
+        this.setY(this.getY()+ (int) this.directionY);
+    }
 }
