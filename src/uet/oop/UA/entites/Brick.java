020df7fafa67e942a1f93cbdb13baed0f6ab09c2
duckhiem5a3-hue <duckhiem5a3@gmail.com>
2025-10-19 09:05:50 +0700

    make a void createBrickGrid method with List<GameObject> parameter, used to instantly create a grid of brick objects and add them to list of game objects

--- src/uet/oop/UA/entites/Brick.java
+++ src/uet/oop/UA/entites/Brick.java
@@ -9,4 +9,30 @@ public class Brick extends GameObject {
         this.set_Drawed_Paddle_image();
         this.hitPoints = 2;
     }
+    public static void createBrickGrid(List<GameObject> Brick_List) {
+        Brick[][] bricks = new Brick[4][8];
+        int startX = 100;
+        int startY = 100;
+        int colorcode;
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 8; col++) {
+                switch (col % 4) {
+                    case 0 -> colorcode = 11; // red
+                    case 1 -> colorcode = 2;  // blue
+                    case 2 -> colorcode = 6;  // green
+                    case 3 -> colorcode = 13; // yellow
+                    default -> colorcode = 12; // white
+                }
+                //conditions for row and col can be used to create not-rectangular patterns of bricks
+                //for example, skip bricks at (1,1), (2,2), (3,3)
+                bricks[row][col] = new Brick(startX + col * BRICK_WIDTH, startY + row * BRICK_HEIGHT, colorcode);
+            }
+        }
+        // Add bricks to the GamePanel's objectList
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 8; col++) {
+                Brick_List.add(bricks[row][col]);
+            }
+        }
+    }
 }
