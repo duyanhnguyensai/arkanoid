d8f456cf765927ac648f37e765cd4fd3c8914a6c
duyanhnguyensai <duyanhnguyensai@gmail.com>
2025-10-19 13:51:26 +0700

    fix: move handleWallCollision to class Ball

--- src/uet/oop/UA/entites/Ball.java
+++ src/uet/oop/UA/entites/Ball.java
@@ -9,6 +9,8 @@ public class Ball extends MovableObject {
     protected double motionAngle;
     protected double cosaAngle;
     protected double sinAngle;
+    public static final int GAME_WIDTH = 900;
+    public static final int GAME_HEIGHT = 800;
 
     public void move(double angle) {
         this.motionAngle = angle;
@@ -23,4 +25,24 @@ public class Ball extends MovableObject {
         this.setX(this.getX()+ (int) this.directionX);
         this.setY(this.getY()+ (int) this.directionY);
     }
+
+    public void handleWallCollision() {
+        // tường 2 bên
+        if (this.getX() <= 0) { //kiểm tra tọa độ x của bóng có vượt qua biên trái không
+            this.cosaAngle *= -1;
+            this.setX(0);
+        } else if (this.getX() >= GAME_WIDTH - this.getWidth()) {
+            this.cosaAngle *= -1;
+            this.setX(GAME_WIDTH - this.getWidth());
+        }
+        // tường trên, dưới
+        if (this.getY() <= 0) {
+            this.sinAngle *= -1;
+            this.setY(0);
+        } else if (this.getY() >= GAME_HEIGHT - this.getHeight()) {
+            this.sinAngle *= -1;
+            this.setY(GAME_HEIGHT - this.getHeight());
+        }
+    }
+
 }
