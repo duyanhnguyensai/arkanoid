c44fff20fea4d33edb6c9567b73eb518f8faa597
duyanhnguyensai <duyanhnguyensai@gmail.com>
2025-11-03 01:03:41 +0700

    feat: update collision ball

--- src/uet/oop/UA/entites/Ball.java
+++ src/uet/oop/UA/entites/Ball.java
@@ -247,6 +247,272 @@ public class Ball extends MovableObject {
         return isNewCollision;
     }
 
+    public int findPosibleCollisionPoint(double pX, double pY, double cX1, double cY1, double cX2, double cY2) {
+        int motionCase = motionPath.nearStart(pX, pY, cX1, cY1, cX2, cY2
+                , true, this.getCentralX(), this.getCentralY());
+        if (motionCase == 1) {
+            return 1;
+        } else if (motionCase == 2) {
+            return 0;
+        }  else {
+            return -1;
+        }
+    }
+
+    public int isLeftCollision(GameObject obj) {
+        int leftcollision = -1;
+        double pY = this.preCentralY;
+        double pX = this.preCentralX;
+        motionPath.calculateCoefficient(pX, pY, this.getCentralX(), this.getCentralY());
+        double cX;
+        double cY;
+
+        if (abs(this.getCentralY() - pY)  <= 0.0001 && abs(this.getCentralX() - pX)  <= 0.0001) {
+            return -1;
+        } else if (abs(this.getCentralX() - pX)  <= 0.0001) {
+            return -1;
+        } else if (abs(this.getCentralY() - pY)  <= 0.0001) {
+            double cX1 = obj.getX() - (double) this.getWidth() /2;
+            double cX2 = obj.getX() + obj.getWidth() + (double) this.getHeight() /2;
+            cY = this.getCentralY();
+            int motionCase = motionPath.nearStart(pX, pY, cX1, cY, cX2, cY
+                    , false, this.getCentralX(), this.getCentralY());
+            if (motionCase == 1) {
+                cX = cX1;
+                leftcollision = 1;
+            } else if (motionCase == 2) {
+                cX = cX2;
+                leftcollision = 0;
+            } else {
+                return -1;
+            }
+        } else {
+            double cX1 = obj.getX() - (double) this.getWidth() /2;
+            double cY1 = motionPath.getA() * cX1 + motionPath.getB();
+            double cX2 = obj.getX() + obj.getWidth() + (double) this.getHeight() /2;
+            double cY2 = motionPath.getA() * cX2 + motionPath.getB();
+            int posibleCase = findPosibleCollisionPoint(pX, pY, cX1, cY1, cX2, cY2);
+            if (posibleCase == 1) {
+                cX = cX1;
+                cY = cY1;
+                leftcollision = 1;
+            } else if (posibleCase == 2) {
+                cX = cX2;
+                cY = cY2;
+                leftcollision = 0;
+            }  else {
+                return -1;
+            }
+        }
+        if (cY >= obj.getY() && cY <= obj.getY() + obj.getHeight()) {
+            this.collisionCentralX = cX;
+            this.collisionCentralY = cY;
+            return leftcollision;
+        }
+        return -1;
+    }
+
+    public int isUpCollision(GameObject obj) {
+        int upcollision = -1;
+        double pY = this.preCentralY;
+        double pX = this.preCentralX;
+        System.out.println("pY: " + pY + " pX: " + pX + this.getCentralX() + " " + this.getCentralY());
+        motionPath.calculateCoefficient(pX, pY, this.getCentralX(), this.getCentralY());
+        double cX;
+        double cY;
+
+        if (abs(this.getCentralY() - pY)  <= 0.0001 && abs(this.getCentralX() - pX)  <= 0.0001) {
+            return -1;
+        } else if (abs(this.getCentralX() - pX)  <= 0.0001) {
+            double cY1 = obj.getY() - (double) this.getWidth() /2;
+            double cY2 = obj.getY() + obj.getHeight() + (double) this.getHeight() /2;
+            cX = this.getCentralX();
+            int motionCase = motionPath.nearStart(pX, pY, cX, cY1, cX, cY2
+                    , false, this.getCentralX(), this.getCentralY());
+            if (motionCase == 1) {
+                cY = cY1;
+                upcollision = 1;
+            } else if (motionCase == 2) {
+                cY = cY2;
+                upcollision = 0;
+            } else {
+                return -1;
+            }
+        } else if (abs(this.getCentralY() - pY)  <= 0.0001) {
+            return -1;
+        } else {
+            double cY1 = obj.getY() - (double) this.getHeight() /2;
+            double cX1 = (cY1 - motionPath.getB()) / motionPath.getA();
+            double cY2 = obj.getY() + obj.getHeight() + (double) this.getHeight() /2;
+            double cX2 = (cY2 - motionPath.getB()) / motionPath.getA();
+            int posibleCase = findPosibleCollisionPoint(pX, pY, cX1, cY1, cX2, cY2);
+
+            if (posibleCase == 1) {
+                cX = cX1;
+                cY = cY1;
+                upcollision = 1;
+            } else if (posibleCase == 2) {
+                cX = cX2;
+                cY = cY2;
+                upcollision = 0;
+            }  else {
+                return -1;
+            }
+        }
+
+        if (cX >= obj.getX() && cX <= obj.getX() + obj.getWidth()) {
+            this.collisionCentralX = cX;
+            this.collisionCentralY = cY;
+            return upcollision;
+        }
+
+        return -1;
+    }
+
+    public boolean inCorner(GameObject obj, double cX, double cY, int corner) {
+
+        if (corner == 1) {
+            return cX < obj.getX() && cY < obj.getY();
+        } else if (corner == 2) {
+            return cX > obj.getX() + obj.getWidth() && cY < obj.getY();
+        } else if (corner == 3) {
+            return cX < obj.getX() && cY > obj.getY() +  obj.getHeight();
+        } else if (corner == 4) {
+            return  cX > obj.getX() + obj.getWidth() && cY > obj.getY() +  obj.getHeight();
+        }
+
+        return false;
+    }
+
+    public int isCornerCollision_(GameObject obj, double X0, double Y0, int corner) {
+
+        int cornercollision = -1;
+        double pY = this.preCentralY;
+        double pX = this.preCentralX;
+        motionPath.calculateCoefficient(pX, pY, this.getCentralX(), this.getCentralY());
+        double cX;
+        double cY;
+
+        if (abs(this.getCentralY() - pY)  <= 0.0001 && abs(this.getCentralX() - pX)  <= 0.0001) {
+            return -1;
+        } else if (abs(this.getCentralX() - pX)  <= 0.0001) {
+            cX = this.getCentralX();
+            double cY1;
+            double cY2;
+
+            if ((double) (this.getWidth() * this.getWidth()) / 4
+                    - (cX - X0) * (cX - X0) >= 0) {
+                if (abs(cX - X0 + (double) this.getWidth() / 2) <= 0.0001) {
+                    return -1;
+                }
+                cY1 = sqrt((double) (this.getWidth() * this.getWidth()) / 4
+                        - (cX - X0) * (cX - X0)) + Y0;
+                cY2 = -sqrt((double) (this.getWidth() * this.getWidth()) / 4
+                        - (cX - X0) * (cX - X0)) + Y0;
+                if (!inCorner(obj, cX, cY1, corner) && !inCorner(obj, cX, cY2, corner)) {
+                    return -1;
+                }
+            } else {
+                return -1;
+            }
+            int motionCase = motionPath.nearStart(pX, pY, cX, cY1, cX, cY2
+                    , false, this.getCentralX(), this.getCentralY());
+
+            if (motionCase == 1 && inCorner(obj, cX, cY1, corner)) {
+                cY = cY1;
+                cornercollision = 1;
+            } else if (motionCase == 2 && inCorner(obj, cX, cY2, corner)) {
+                cY = cY2;
+                cornercollision = 0;
+            } else {
+                return -1;
+            }
+        } else if (abs(this.getCentralY() - pY)  <= 0.0001) {
+            cY = this.getCentralY();
+            double cX1;
+            double cX2;
+
+            if ((double) (this.getWidth() * this.getWidth()) / 4
+                    - (cY - Y0) * (cY - Y0) >= 0) {
+                if (abs(cY - Y0 + (double) this.getWidth() /2) <= 0.0001) {
+                    return -1;
+                }
+                cX1 = sqrt((double) (this.getWidth() * this.getWidth()) / 4
+                        - (cY - Y0) * (cY - Y0)) + X0;
+                cX2 = -sqrt((double) (this.getWidth() * this.getWidth()) / 4
+                        - (cY - Y0) * (cY - Y0)) + X0;
+                if (!inCorner(obj, cX1, cY, corner) && !inCorner(obj, cX2, cY, corner)) {
+                    return -1;
+                }
+            } else {
+                return -1;
+            }
+            int motionCase = motionPath.nearStart(pX, pY, cX1, cY, cX2, cY
+                    , false, this.getCentralX(), this.getCentralY());
+            if (motionCase == 1 && inCorner(obj, cX1, cY, corner)) {
+                cX = cX1;
+                cornercollision = 1;
+            } else if (motionCase == 2 && inCorner(obj, cX2, cY, corner)) {
+                cX = cX2;
+                cornercollision = 0;
+            } else {
+                return -1;
+            }
+        } else {
+            CollisionEquation collisionEquation = new CollisionEquation(motionPath, X0, Y0
+                    , (double) this.getWidth() /2);
+            double cX1;
+            double cY1;
+            double cX2;
+            double cY2;
+            collisionEquation.solve();
+
+            if (collisionEquation.getDelta() < 0) {
+                return -1;
+            }
+
+            cX1 = collisionEquation.getX1();
+            cY1 = motionPath.getA() * cX1 + motionPath.getB();
+            cX2 = collisionEquation.getX2();
+            cY2 = motionPath.getA() * cX2 + motionPath.getB();
+            int motionCase = motionPath.nearStart(pX, pY, cX1, cY1, cX2, cY2
+                    , true, this.getCentralX(), this.getCentralY());
+
+            if (motionCase == 1 && inCorner(obj, cX1, cY1, corner)) {
+                cX = cX1;
+                cY = cY1;
+                cornercollision = 1;
+            } else if (motionCase == 2 && inCorner(obj, cX2, cY2, corner)) {
+                cX = cX2;
+                cY = cY2;
+                cornercollision = 0;
+            }  else {
+                return -1;
+            }
+        }
+
+        if (inCorner(obj, cX, cY, corner)) {
+            this.collisionCentralX = cX;
+            this.collisionCentralY = cY;
+        }
+
+        return cornercollision;
+    }
+
+    public int isCornerCollision(GameObject obj) {
+        if (isCornerCollision_(obj, obj.getX(), obj.getY(),1) != -1) {
+            return 1;
+        } else if (isCornerCollision_(obj, obj.getX() + obj.getWidth(), obj.getY(), 2) != -1) {
+            return 2;
+        } else if (isCornerCollision_(obj, obj.getX(), obj.getY() + obj.getHeight(), 3) != -1) {
+            return 3;
+        } else if (isCornerCollision_(obj, obj.getX() + obj.getWidth(), obj.getY() + obj.getHeight(), 4) != -1) {
+            return 4;
+        }
+
+        return -1;
+    }
+
     public int handleBrickCollision(GameObject obj) {
         if (this.isPossibleToCollision(obj)) {
             int upcollisionb = this.isUpCollision(obj);
